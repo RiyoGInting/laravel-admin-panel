e7d42300a1bc70c9ce5953df3d41a4703c42fac5
riyo <riyoginting@gmail.com>
2021-08-03 08:32:54 +0700

    laravel-excel

--- app/Exports/CompaniesExport.php
+++ app/Exports/CompaniesExport.php
@@ -4,14 +4,27 @@ namespace App\Exports;
 
 use App\Models\Company;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class CompaniesExport implements FromCollection
+class CompaniesExport implements FromCollection, ShouldAutoSize, WithHeadings
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
         return Company::all();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'No',
+            'Name',
+            'Email',
+            'Logo',
+            'Website',
+        ];
     }
 }
--- app/Exports/EmployeesExport.php
+++ app/Exports/EmployeesExport.php
@@ -4,14 +4,27 @@ namespace App\Exports;
 
 use App\Models\Employee;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class EmployeesExport implements FromCollection
+class EmployeesExport implements FromCollection, ShouldAutoSize, WithHeadings
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
         return Employee::all();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Company ID',
+            'First Name',
+            'Last Name',
+            'Email',
+            'Phone',
+        ];
     }
 }
